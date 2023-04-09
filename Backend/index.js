7b15a33d52453e9eb0a7a0aa5f1f1b123cb72938
MaiyonDevWindows <maiyonaisu1102@gmail.com>
2023-04-09 21:32:23 +0700

    ConnectMySql-done-Quan

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+
 require('dotenv').config();
-const port = process.env.PORT;
 const dbConfig = require('./src/configs/db.config');
 const route = require("./src/routes/routes");
-
 const app = express();
-
-app.use(express.json());
-
-app.use(cors());
-
 app.use(morgan('combined')); // Server logger
 
-dbConfig.connectDB(); // Connect DB
-
+const port = process.env.PORT || 3000;
+app.use(express.json());
+app.use(cors());
 route(app); // routes
 
+dbConfig.connectDb(); // Connect Database
+
+app.get('/Trains', function (req, res) {
+    var sql = "SELECT * FROM Train";
+    dbConfig.db.query(sql, function (err, results) {
+        if (err) throw err;
+        res.send(results);
+    });
+});
+
+app.get('/', (req, res) => res.send('Hello World!'));
 app.listen(port, () => console.log(`App listening at https://localhost:${port}`));
