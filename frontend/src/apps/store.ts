0edf2136f28a934d57081da37a5fe8b9980f54bf
TienCNTT <20200892@eaut.edu.vn>
2023-04-21 08:10:57 +0700

    fix branch

--- frontend/src/apps/store.ts
+++ frontend/src/apps/store.ts
@@ -1,22 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit'
 // import { postApi } from './services/postApi'
 import { userApi } from './services/userApi'
-import { shipApi } from './services/shipApi'
+import { trainApi } from './services/trainApi'
 import { scheduleApi } from './services/scheduleApi'
-import { routeApi } from './services/routeApi'
+import { trainStationApi } from './services/trainStationApi'
 import authSliceReducer from './slices/authSlice'
 
 export const store = configureStore({
   reducer: {
     // [postApi.reducerPath]: postApi.reducer,
     [userApi.reducerPath]: userApi.reducer,
-    [shipApi.reducerPath]: shipApi.reducer,
+    [trainApi.reducerPath]: trainApi.reducer,
     [scheduleApi.reducerPath]: scheduleApi.reducer,
-    [routeApi.reducerPath]: routeApi.reducer,
+    [trainStationApi.reducerPath]: trainStationApi.reducer,
     auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userApi.middleware, shipApi.middleware, scheduleApi.middleware, routeApi.middleware),
+    getDefaultMiddleware().concat(userApi.middleware, trainApi.middleware, scheduleApi.middleware, trainStationApi.middleware),
 })
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
