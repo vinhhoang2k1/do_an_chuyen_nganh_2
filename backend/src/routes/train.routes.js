0266917a895c56b2e46a82823730ea973ad9a460
tuanpham <tuanpham24.it@gmail.com>
2023-04-27 15:59:15 +0700

    add swagger-api docs for front-end

--- backend/src/routes/train.routes.js
+++ backend/src/routes/train.routes.js
@@ -3,9 +3,82 @@ const router = express.Router();
 const trainController = require("../controllers/train.controller");
 const authMiddleware = require("../middlewares/auth.middleware")
 
+/**
+ * @swagger
+ * /api/train:
+ *   post:
+ *     summary: Create a new Train
+ *     tags: [Trains API]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *     responses:
+ *       200:
+ *         description: The list of the Trains
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ */
 router.post("/", authMiddleware.verifyToken, trainController.createSingleTrain);
-router.get("/",authMiddleware.verifyToken, trainController.getAllTrain);
-router.put("/:id",authMiddleware.verifyToken, trainController.updateSingleTrain);
-router.delete("/:id",authMiddleware.verifyToken, trainController.deleteSingleTrain);
+
+/**
+ * @swagger
+ * /api/train:
+ *   get:
+ *     summary: Lists all the Trains
+ *     tags: [Trains API]
+ *     responses:
+ *       200:
+ *         description: The list of the Trains
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ */
+router.get("/", authMiddleware.verifyToken, trainController.getAllTrain);
+
+/**
+ * @swagger
+ * /api/train:
+ *   put:
+ *     summary: Update single Train
+ *     tags: [Trains API]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *     responses:
+ *       200:
+ *         description: The list of the Trains
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ */
+router.put("/:id", authMiddleware.verifyToken, trainController.updateSingleTrain);
+
+/**
+ * @swagger
+ * /api/train:
+ *   delete:
+ *     summary: Remove single Train
+ *     tags: [Trains API]
+ *     responses:
+ *       200:
+ *         description: The list of the Trains
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ */
+router.delete("/:id", authMiddleware.verifyToken, trainController.deleteSingleTrain);
 
 module.exports = router;
